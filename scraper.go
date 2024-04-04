d8b042d12c186fa6fba44edc9c77649afbb63136
Jatin Yadav <Jatin.Yadav@wuh-group.com>
2024-04-05 01:20:49 +0530

    Create Json file of the output

--- scraper.go
+++ scraper.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"os"
+
 	"github.com/gocolly/colly"
 	"github.com/kr/pretty"
 )
 
 type details struct {
-	header string
-	desc   string
-	href   string
+	Header string `json:"header"`
+	Desc   string `json:"desc"`
+	Href   string `json:"href"`
 }
 
 func main() {
@@ -19,18 +23,30 @@ func main() {
 	// Find and visit all links
 	c.OnHTML("div.three div.row div.column", func(e *colly.HTMLElement) {
 		items := details{
-			header: e.ChildText("h2.ui"),
-			desc:   e.ChildText("p"),
-			href:   e.ChildAttr("a", "href"),
+			Header: e.ChildText("h2.ui"),
+			Desc:   e.ChildText("p"),
+			Href:   e.ChildAttr("a", "href"),
 		}
 		allitems = append(allitems, items)
-
 	})
-
-	// c.OnRequest(func(r *colly.Request) {
-	// 	fmt.Println("Visiting", r.URL)
-	// })
 
 	c.Visit("http://go-colly.org/")
 	pretty.Print(allitems)
+
+	// Marshal the struct array into JSON bytes
+	jsonData, err := json.Marshal(allitems)
+	if err != nil {
+		fmt.Println("Error marshaling JSON:", err)
+		return
+	}
+
+	// Write JSON data to a file
+	err = os.WriteFile("products.json", jsonData, 0644)
+	if err != nil {
+		fmt.Println("Error writing JSON to file:", err)
+		return
+	}
+
+	fmt.Println("JSON data written to products.json successfully")
+
 }
