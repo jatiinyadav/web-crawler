b64ccf6537546a40e5c9014cdec2a8b785bd3a3e
externref <shiva02939@gmail.com>
2024-06-19 18:35:49 +0530

    add animations and styling

--- ui/src/lib/Landing.svelte
+++ ui/src/lib/Landing.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
-  // import { onMount } from "svelte";
+  import { onMount } from "svelte";
   import type { IResponseType } from "../interfaces/IResponseType";
-
+  import { fade, slide } from "svelte/transition";
+  let mount: boolean = false;
+  onMount(() => {
+    mount = true;
+  });
   // let message = "";
   let items: IResponseType[] = [];
   // const endpoint = "http://localhost:8080/api/message";
@@ -26,11 +30,11 @@
       console.log(inputValue1);
 
       const response = await fetch(
-        `http://localhost:8080/api/weburl?url1=${inputValue1}&url2=${inputValue2}`
+        `http://localhost:8080/api/weburl?url1=${inputValue1}&url2=${inputValue2}`,
       );
       const data = await response.json();
       console.log(data); // Process the response data as needed
-      items = data
+      items = data;
     } catch (error) {
       console.error("Error fetching data:", error);
     }
@@ -40,35 +44,61 @@
   }
 </script>
 
-<main>
-  <!-- <h1>{message}</h1> -->
+{#if mount}
+  <main>
+    <!-- <h1>{message}</h1> -->
+    <!-- svelte-ignore a11y-autofocus -->
+    <input type="text" bind:value={inputValue1} />
 
-  <!-- svelte-ignore a11y-autofocus -->
-  <input type="text" bind:value={inputValue1} />
+    <!-- svelte-ignore a11y-autofocus -->
+    <input type="text" bind:value={inputValue2} /><br />
+    <button transition:fade={{ delay: 100, duration: 400 }} on:click={fetchData}
+      >Fetch Data</button
+    >
+   
 
-  <!-- svelte-ignore a11y-autofocus -->
-  <input type="text" bind:value={inputValue2} />
-  <button on:click={fetchData}>Fetch Data</button>
-
-  <h1>List of Items</h1>
-  <ul>
-    {items.length}
-    {#each items as item}
-      <li><span>Header: </span> {item.header}</li>
-      <li><span>Description: </span> {item.desc}</li>
-      <li><span>URL: </span> {item.href}</li>
+    {#if items.length > 0}
+    <button
+    transition:fade={{ delay: 100, duration: 400 }}
+    on:click={() => {
+      items = [];
+    }}>Reset</button
+  >
+      <flex>
+        <div id="loi">List of Items</div>
+        <div>(<b> Total items:</b> {items.length})</div></flex
+      >
       <hr />
-    {/each}
-  </ul>
-</main>
+
+      <ul>
+        <div transition:slide={{ duration: 400, delay: 100, axis: "y" }}>
+          {#each items as item}
+            <div transition:slide={{ duration: 400, delay: 100, axis: "y" }}>
+              <li><span>Header: </span> {item.header}</li>
+              <li><span>Description: </span> {item.desc}</li>
+              <li><span>URL: </span> {item.href}</li>
+              <hr />
+            </div>
+          {/each}
+        </div>
+      </ul>
+    {/if}
+  </main>
+{/if}
 
 <style>
   ul {
     text-align: left;
   }
-
+  input {
+    border-radius: 5%;
+    outline: none;
+  }
   span {
     font-weight: bold;
     font-size: 1.3rem;
+  }
+  #loi {
+    font-size: xx-large;
   }
 </style>
